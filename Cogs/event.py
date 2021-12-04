eee49db88515c8485068abc00ce67ecd9221f219
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-12-05 07:39:45 +0900

    add : 起動メッセージとエラー出力機構の作成

--- Cogs/event.py
+++ Cogs/event.py
@@ -1,1 +1,47 @@
+import discord
+from discord.ext import commands
+import traceback
 
+class AppCmdEvent(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        servers = len(self.bot.guilds)
+        members = 0
+        for guild in self.bot.guilds:
+            members += guild.member_count - 1
+        await self.bot.change_presence(
+            activity=discord.Activity(name=f"Yb!help | {str(servers)}servers | {str(members)}users", type=3)
+        )
+
+    @commands.Cog.listener()
+    async def on_command_error(self,ctx, error):
+        orig_error = getattr(error, "original", error)
+        error_msg = "".join(traceback.TracebackException.from_exception(orig_error).format())
+        if isinstance(error, commands.errors.MissingPermissions):
+            embed = discord.Embed(
+                title="エラー-不明なコマンド",
+                description="不明なコマンドです。`Cu!list`でコマンドを確認してください。\nこのエラーが多発する場合は[公式サーバー](https://discord.gg/RFPQmRnv2j)までお問い合わせください。\n```" + error_msg + "```",
+                colour=0xffa500,
+            )
+            await ctx.reply(embed=embed)
+        elif isinstance(error, commands.errors.MissingPermissions):
+            embed = discord.Embed(
+                title="エラー-権限不足",
+                description="権限が不足しています。権限設定をご確認ください。\nこのエラーが多発する場合は[公式サーバー](https://discord.gg/RFPQmRnv2j)までお問い合わせください。\n```" + error_msg + "```",
+                colour=0xffa500,
+            )
+            await ctx.reply(embed=embed)
+        else:
+            embed = discord.Embed(
+                title="エラー",
+                description="予期せぬエラーが発生しました。\nこのエラーが多発する場合は[公式サーバー](https://discord.gg/RFPQmRnv2j)までお問い合わせください。\n```" + error_msg + "```",
+                colour=0xffa500,
+            )
+            await ctx.reply(embed=embed, mention_author=False)
+
+def setup(bot):
+    return bot.add_cog(AppCmdEvent(bot))
