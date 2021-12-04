8cbf4dad1d72c23abd5ab73f8b2c76e20333684d
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-12-04 18:02:46 +0900

    add : YoutubeTogetherの試作

--- Cogs/main.py
+++ Cogs/main.py
@@ -1,1 +1,26 @@
+from discord.ext import commands
+from discord_together import DiscordTogether
 
+class AppCmdMain(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        servers = len(self.bot.guilds)
+        members = 0
+        for guild in self.bot.guilds:
+            members += guild.member_count - 1
+        await self.bot.change_presence(
+            activity=discord.Activity(name=f"Yb!help | {str(servers)}servers | {str(members)}users", type=3)
+        )
+
+    @commands.command()
+    async def start(self,ctx):
+        link = await self.bot.togetherControl.create_link(ctx.author.voice.channel.id, 'youtube')
+        await ctx.send(f"Click the blue link!\n{link}")
+
+        
+def setup(bot):
+    return bot.add_cog(AppCmdBot(bot))
