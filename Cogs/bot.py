9e03f8740e5db877ad44d5aa127390271ca3bbc1
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-12-05 07:48:11 +0900

    add : bot.pyの作成

---
+++ Cogs/bot.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import asyncio
+import random
+
+class AppCmdBot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+        
+    @commands.command()
+    async def help(self,ctx):
+        embed = discord.Embed(title="ヘルプ", description="comming soon ...", colour=0xff0000)
+        embed.add_field(
+            name=":robot: 》コマンドリスト",
+            value="`help`：ヘルプを表示します。\n"
+                  "`ping`：GeBOTのping値を表示します。\n"
+                  "`youtube`：YouTubeTogetherを開始します。\n"
+                  "`betrayal`：Betrayal.ioを開始します。\n"
+                  "`dice`：サイコロを振れます。",
+        )
+        await ctx.reply(embed=embed)
+
+    @commands.command()
+    async def ping(self,ctx):
+        async with ctx.typing():
+            await asyncio.sleep(0)
+        embed = discord.Embed(title="PING", description=f"ただいまのping値は**{round(self.bot.latency * 1000)}**msです！",
+                              color=0xff0000)
+        await ctx.reply(embed=embed, mention_author=False)
+
+def setup(bot):
+    return bot.add_cog(AppCmdBot(bot))
