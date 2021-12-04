5f2f66e6da46a70315b4c0250ee6c11a0d56772e
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-12-05 07:49:32 +0900

    change : 起動方法を変更

--- discordbot.py
+++ discordbot.py
@@ -1,68 +1,26 @@
 import discord
 from discord.ext import commands
-from discord.http import Route
+import os
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 bot = commands.Bot(
-    command_prefix=["Yb!", "yb!"],
+    command_prefix=["Ge!", "ge!"],
     help_command=None,
     intents=intents,
     allowed_mentions=discord.AllowedMentions(replied_user=False, everyone=False),
     case_insensitive=True
 )
 
-@bot.command('youtube')
-async def youtube(ctx):
-    voice = ctx.author.voice
-    if not voice:
-        return await ctx.send('You have to be in a voice channel to use this command.')
-    r = Route('POST', '/channels/{channel_id}/invites', channel_id=voice.channel.id)
-    payload = {
-        'max_age': 0,
-        'target_type': 2,
-        'target_application_id': 755600276941176913
-    }
-    try:
-        code = (await bot.http.request(r, json=payload))['code']
-    except discord.Forbidden:
-        return await ctx.send('I Need the `Create Invite` permission.')
-    await ctx.send(embed=discord.Embed(description=f'[Click here!](https://discord.gg/{code})', color=0x2F3136))
 
-@bot.command('betrayal')
-async def betrayal(ctx):
-    voice = ctx.author.voice
-    if not voice:
-        return await ctx.send('You have to be in a voice channel to use this command.')
-    r = Route('POST', '/channels/{channel_id}/invites', channel_id=voice.channel.id)
-    payload = {
-        'max_age': 0,
-        'target_type': 2,
-        'target_application_id': 773336526917861400
-    }
-    try:
-        code = (await bot.http.request(r, json=payload))['code']
-    except discord.Forbidden:
-        return await ctx.send('I Need the `Create Invite` permission.')
-    await ctx.send(embed=discord.Embed(description=f'[Click here!](https://discord.gg/{code})', color=0x2F3136))
-    
-@bot.command('fishington')
-async def fishington(ctx):
-    voice = ctx.author.voice
-    if not voice:
-        return await ctx.send('You have to be in a voice channel to use this command.')
-    r = Route('POST', '/channels/{channel_id}/invites', channel_id=voice.channel.id)
-    payload = {
-        'max_age': 0,
-        'target_type': 2,
-        'target_application_id': 814288819477020702
-    }
-    try:
-        code = (await bot.http.request(r, json=payload))['code']
-    except discord.Forbidden:
-        return await ctx.send('I Need the `Create Invite` permission.')
-    await ctx.send(embed=discord.Embed(description=f'[Click here!](https://discord.gg/{code})', color=0x2F3136))
-    
+token = os.environ["token"]
 
+def restart_bot():
+  os.execv(sys.executable, ['python'] + sys.argv)
 
-bot.run("OTE2NjA5Mzc2OTkzODMyOTkx.YaspAQ.a7cKXVTMGMgm2E4Qpkx6actPK2U")
+bot.load_extension("jishaku")
+
+bot.load_extension("Cogs.event")
+bot.load_extension("Cogs.bot")
+
+bot.run(token)
