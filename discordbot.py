11ca41fb4e5e2f91f3de057ba867e74be421ee98
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-12-05 07:49:57 +0900

    fix : プレフィックスの修正

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,7 @@ import os
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 bot = commands.Bot(
-    command_prefix=["Ge!", "ge!"],
+    command_prefix=["Yb!", "yb!"],
     help_command=None,
     intents=intents,
     allowed_mentions=discord.AllowedMentions(replied_user=False, everyone=False),
