b0bbafe228370531d3bd9279245b2768d88a851e
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-12-04 17:57:25 +0900

    add : botのセットアップ

--- discordbot.py
+++ discordbot.py
@@ -1,1 +1,26 @@
+import discord
+from discord.ext import commands
+import os
+import sys
 
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(
+    command_prefix=["Yb!", "Yb!"],
+    help_command=None,
+    intents=intents,
+    allowed_mentions=discord.AllowedMentions(replied_user=False, everyone=False),
+    case_insensitive=True
+)
+
+
+token = os.environ["token"]
+
+def restart_bot():
+  os.execv(sys.executable, ['python'] + sys.argv)
+
+bot.load_extension("jishaku")
+
+bot.load_extension("Cogs.main")
+
+bot.run(token)
