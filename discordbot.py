d7b2b81bf1e2358934557d171f0f09620c333cbf
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-12-11 20:09:25 +0900

    change : admin.pyのロード

--- discordbot.py
+++ discordbot.py
@@ -23,5 +23,6 @@ bot.load_extension("jishaku")
 bot.load_extension("Cogs.event")
 bot.load_extension("Cogs.bot")
 bot.load_extension("Cogs.general")
+bot.load_extension("Cogs.admin")
 
 bot.run(token)
